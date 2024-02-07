f8b393e2a28b73bedf2bb4b216759b0823417906
Cristiano Sobral <cssobral2013@gmail.com>
2024-02-07 01:09:09 -0300

    Update

---
+++ generate-woff-woff2.py
@@ -0,0 +1,230 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/Reijima-Thin.ttf'
+woffPath = './web/Reijima-Thin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Reijima-ThinItalic.ttf'
+woffPath = './web/Reijima-ThinItalic.woff'
+
+    
+srcPath  = './ttf/Reijima-Light.ttf'
+woffPath = './web/Reijima-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Reijima-LightItalic.ttf'
+woffPath = './web/Reijima-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Reijima-LightItalic.ttf'
+woffPath = './web/Reijima-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Reijima-Regular.ttf'
+woffPath = './web/Reijima-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Reijima-Italic.ttf'
+woffPath = './web/Reijima-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Reijima-Medium.ttf'
+woffPath = './web/Reijima-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Reijima-MediumItalic.ttf'
+woffPath = './web/Reijima-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Reijima-Bold.ttf'
+woffPath = './web/Reijima-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Reijima-BoldItalic.ttf'
+woffPath = './web/Reijima-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Reijima-ExtraBold.ttf'
+woffPath = './web/Reijima-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Reijima-ExtraBoldItalic.ttf'
+woffPath = './web/Reijima-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Reijima-Black.ttf'
+woffPath = './web/Reijima-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Reijima-BlackItalic.ttf'
+woffPath = './web/Reijima-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/Reijima-Thin.ttf'
+woff2Path = './web/Reijima-Thin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Reijima-ThinItalic.ttf'
+woff2Path = './web/Reijima-ThinItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/Reijima-Light.ttf'
+woff2Path = './web/Reijima-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Reijima-LightItalic.ttf'
+woff2Path = './web/Reijima-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Reijima-Regular.ttf'
+woff2Path = './web/Reijima-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Reijima-Italic.ttf'
+woff2Path = './web/Reijima-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Reijima-Medium.ttf'
+woff2Path = './web/Reijima-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Reijima-MediumItalic.ttf'
+woff2Path = './web/Reijima-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Reijima-Bold.ttf'
+woff2Path = './web/Reijima-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Reijima-BoldItalic.ttf'
+woff2Path = './web/Reijima-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Reijima-ExtraBold.ttf'
+woff2Path = './web/Reijima-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Reijima-ExtraBoldItalic.ttf'
+woff2Path = './web/Reijima-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Reijima-Black.ttf'
+woff2Path = './web/Reijima-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Reijima-BlackItalic.ttf'
+woff2Path = './web/Reijima-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
